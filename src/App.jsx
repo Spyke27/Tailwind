ce18dec6589ff18b789a435fc6685d7a7d90c121
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-02-05 23:26:03 -0300

    Treinando Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
    <>
-    <div className='bg-black text-yellow-500 flex justify-center'>Texto Qualquer</div>
+    <div className='flex justify-between items-center bg-black py-5 px-10'>
+      <h1 className='text-white'>Texto</h1>
+      <button className='px-3 py-2 text-white bg-red-500 rounded-xl'>Clique Aqui</button>
+    </div>
    </>
   )
 }
